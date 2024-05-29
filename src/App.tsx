6ab97b8dca104c92c0c67d7d0d66ad3c91ba1499
mohaimenkhalid <mohaimen707@gmail.com>
2024-05-30 02:03:29 +0600

    feature: add user list for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
 import './App.css'
 import Greet from "./components/greet/Greet.tsx";
 import User from "./components/user/User.tsx";
+import UserList from "./components/user-list/UserList.tsx";
 
 function App() {
+    type User = {
+        id: number,
+        name: string,
+        email: string,
+        phone: string,
+        age: number,
+        balance: number
+    }
     const user = {name: "khalid", age: 12, isAdmin: true }
+
+    const users: User[] = [
+        {
+            id: 1,
+            name: "mohaimen",
+            email: "mohaimen707@gmail.com",
+            phone: "01798659099",
+            age: 30,
+            balance: 50000
+        },
+        {
+            id: 2,
+            name: "mamun",
+            email: "mamun@gmail.com",
+            phone: "01798659057",
+            age: 42,
+            balance: 700000,
+        }
+    ]
   return (
     <>
       <Greet  name="mohaimen"/>
@@ -11,6 +39,10 @@ function App() {
         <hr />
 
         <User user={user} />
+
+        <hr />
+
+        <UserList users={users} />
     </>
   )
 }
---
+++ src/components/user-list/UserList.tsx
@@ -0,0 +1,23 @@
+type User = {
+    id: number,
+    name: string,
+    email: string,
+    phone: string,
+    age: number,
+    balance: number
+}
+const UserList = ({ users } : { users: User[] }) => {
+
+    if(users.length === 0) return <p>No users available.</p>
+    return (
+        <ul>
+            { users.map((user: User) => (
+                <li key={user.id}>
+                    <a href={`/users/${user.id}`}>{user.name}</a>
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+export default UserList;
