2a9a2e6259961c49b68ac38b6bc922785b730607
mohaimen <mohaimen.jatri@gmail.com>
2024-05-29 17:50:57 +0600

    user component ready for test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
 import Greet from "./components/greet/Greet.tsx";
+import User from "./components/user/User.tsx";
 
 function App() {
+    const user = {name: "khalid", age: 12, isAdmin: true }
   return (
     <>
-      <Greet />
+      <Greet  name="mohaimen"/>
+
+        <hr />
+
+        <User user={user} />
     </>
   )
 }
---
+++ src/components/user/User.tsx
@@ -0,0 +1,17 @@
+type User = {name: string, age: number, isAdmin: boolean}
+const User = ({user} : {user: User}) => {
+    return (
+        <>
+            <h2>User profile</h2>
+            {
+                user.isAdmin && <button>Edit Profile</button>
+            }
+            <div>
+                <strong>Name:</strong> {user.name},
+                <strong>Age:</strong> {user.age}
+            </div>
+        </>
+    );
+};
+
+export default User;
