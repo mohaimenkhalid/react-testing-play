12e531d722841762552f355f889efe85232ef0f0
mohaimenkhalid <mohaimen707@gmail.com>
2024-06-04 21:47:34 +0600

    added expanded text component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import User from "./components/user/User.tsx";
 import UserList from "./components/user-list/UserList.tsx";
 import ImageGallery from "./components/image/ImageGallery.tsx";
 import TermsAndConditions from "./components/TermsAndConditions/TermsAndConditions.tsx";
+import ExpendableText from "./components/ExpendableText.tsx";
 
 function App() {
     type User = {
@@ -52,9 +53,17 @@ function App() {
 
             <ImageGallery images={images}/>
 
-            <hr />
+            <hr/>
 
-            <TermsAndConditions />
+            <TermsAndConditions/>
+
+            <hr/>
+
+            <div style={{width: "100%"}}>
+                <ExpendableText
+                    text="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+                />
+            </div>
         </>
     )
 }
